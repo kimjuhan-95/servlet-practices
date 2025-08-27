d20a01e5bc4a84b2734a28a897e50d113191eaf9
김주한 <jhkim9596@gmail.com>
2025-08-27 15:47:45 +0900

    servlet

---
+++ helloweb/src/main/java/helloweb/CookieServlet.java
@@ -0,0 +1,10 @@
+package helloweb;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieServlet {
+
+}
